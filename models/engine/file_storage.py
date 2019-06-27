167371d8385ef7134d4afd478835a154449b0eba
Tu Vo <tuvo1106@gmail.com>
2019-06-27 21:39:12 +0000

    Start edits to file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+"""
+This module contains the prototype for BaseModel class.
+"""
+
+
+class FileStorage:
+    """FileStorage class."""
+
+    def __init__(self):
+        """Initialize FileStorage."""
+
