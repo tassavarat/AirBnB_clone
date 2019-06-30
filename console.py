c39fe2aed2d755b20ef51020e5e810b4d4e030e9
tassavarat <tassavarat@gmail.com>
2019-06-30 14:46:02 -0700

    Added imports

--- console.py
+++ console.py
@@ -3,6 +3,10 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 import models
 
 
