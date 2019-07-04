13f9ecaf19057e066ed507ca3ba333b3f694411b
tassavarat <tassavarat@gmail.com>
2019-07-03 19:20:58 -0700

    Reworked do_update

--- console.py
+++ console.py
@@ -106,10 +106,11 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         elif len(params) == 1:
             print("** instance id missing **")
-        elif len(params) == 2:
+        elif len(params) > 1:
             k = params[0] + "." + params[1]
             if k not in models.storage.all():
                 return(print("** no instance found **"))
+        if len(params) == 2:
             print("** attribute name missing **")
         elif len(params) == 3:
             print("** value missing **")
@@ -127,8 +128,6 @@ class HBNBCommand(cmd.Cmd):
                 pass
             if k in models.storage.all():
                 setattr(models.storage.all()[k], params[2], params[3])
-            else:
-                return(print("** no instance found **"))
 
     def do_count(self, args):
         """Retrieves the number of instances of a class"""
